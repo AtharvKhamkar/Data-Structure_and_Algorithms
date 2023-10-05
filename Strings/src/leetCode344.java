37e826e5ad355ef6ae0ddba34cd7099b719d489e
AtharvKhamkar <khamkaratharv2002@gmail.com>
2023-10-05 20:33:10 +0530

    binarySearchQuestions

---
+++ Strings/src/leetCode344.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class leetCode344 {
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        reverseString(s);
+        System.out.println(Arrays.toString(s));
+
+    }
+    public static void reverseString(char[] s) {
+        int start = 0;
+        int end = s.length-1;
+        while(start<end){
+            swap(s,start,end);
+            start++;
+            end--;
+        }
+    }
+
+    public static void swap(char[] s,int first,int second){
+        char temp = s[first];
+        s[first] = s[second];
+        s[second] = temp;
+    }
+
+
+}
