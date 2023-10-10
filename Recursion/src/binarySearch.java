6a845216512c5e9dae973199892d96c348c9c66e
AtharvKhamkar <khamkaratharv2002@gmail.com>
2023-10-11 00:37:28 +0530

    Recursion

---
+++ Recursion/src/binarySearch.java
@@ -0,0 +1,24 @@
+public class binarySearch {
+    public static void main(String[] args) {
+        int[] arr = {1,4,9,88,95,99};
+        int target = 99;
+        System.out.println(BS(arr,target,0,arr.length-1));
+
+    }
+
+    public static int BS(int[] arr,int target,int s,int e){
+        if(s>e){
+            return -1;
+        }
+
+        int m = s + (e-s)/2;
+
+        if(arr[m] == target){
+            return m;
+        }
+        if(arr[m] < target){
+            return BS(arr,target,m+1,e);
+        }
+        return BS(arr,target,s,m-1);
+    }
+}
