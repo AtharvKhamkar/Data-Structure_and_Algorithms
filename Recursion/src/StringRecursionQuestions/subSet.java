0015679684a9ee5bfb4f7c59fb425bd931139a0f
AtharvKhamkar <khamkaratharv2002@gmail.com>
2023-11-23 20:34:23 +0530

    Recursion Problem

--- Recursion/src/StringRecursionQuestions/subSet.java
+++ Recursion/src/StringRecursionQuestions/subSet.java
@@ -5,7 +5,7 @@ public class subSet {
     public static void main(String[] args){
         String pro = " ";
         String unpro = "abc";
-        System.out.println(subsetsReturn(pro,unpro));
+        System.out.println(subsetAsciiReturn(pro,unpro));
 
     }
 
@@ -21,6 +21,7 @@ public class subSet {
         subsets(pro,unpro.substring(1));
     }
 
+    //Function to return subset in a ArrayList
     public static ArrayList<String> subsetsReturn(String pro,String unpro){
         if(unpro.isEmpty()){
             ArrayList<String> list = new ArrayList<>();
@@ -34,5 +35,36 @@ public class subSet {
 
         left.addAll(right);
         return left;
+    }
+
+    //Function to Ascii values of subset with subset
+
+    public static void subsetAscii(String pro,String unPro){
+        if(unPro.isEmpty()){
+            System.out.println(pro);
+            return;
+        }
+
+        char Ch = unPro.charAt(0);
+        subsetAscii(pro+Ch,unPro.substring(1));
+        subsetAscii(pro,unPro.substring(1));
+        subsetAscii(pro+(Ch+0),unPro.substring(1));
+    }
+    //Function to Ascii values of subset with subset using return
+    public static ArrayList<String> subsetAsciiReturn(String pro,String unPro){
+        if(unPro.isEmpty()){
+            ArrayList<String> list = new ArrayList<>();
+            list.add(pro);
+            return list;
+        }
+
+        char Ch = unPro.charAt(0);
+        ArrayList<String> first = subsetAsciiReturn(pro+Ch,unPro.substring(1));
+        ArrayList<String> second = subsetAsciiReturn(pro,unPro.substring(1));
+        ArrayList<String> third = subsetAsciiReturn(pro + (Ch+0),unPro.substring(1));
+
+        first.addAll(second);
+        first.addAll(third);
+        return first;
     }
 }
